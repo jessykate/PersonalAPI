ecb3973642ec341796d3f0f0c60840ff11a63ed1
jessykate <jessy.cowansharp@gmail.com>
2010-07-18 13:47:09 -0400

    fleshing out bio handler

--- main.py
+++ main.py
@@ -26,10 +26,20 @@ class IndexHandler(tornado.web.RequestHandler):
 	print application.handlers
         self.render('templates/index.html')
 
+class BaseHandler(tornado.web.RequestHandler):
+	name = 'Jessy Kate Cowan-Sharp'
+	
 
 class BioHandler(tornado.web.RequestHandler):
     def get(self):
-	pass
+	# XXX TODO format=plaintext, format=html
+	date = "2010-07-18" # year-month-day
+	bio = '''Jessy Cowan-Sharp is an urban hacker, an alchemist of social systems, and a communities scientist. She currently works at the Sunlight Foundation, a DC-based open government think tank, writing code to jump start the next generation of participatory citizenry. Previous to Sunlight, Jessy was at NASA working on the Nebula cloud computing platform. Jessy's research interests focus on the study of patterns in human behaviour, organization, and governance; and the development of  platforms to facilitate collaboration and creativity.  She is variously involved in open source software development, open education, intentional housing, cooking and crossfit.  Find her @jessykate or online. '''
+	resp = {'name': 'Jessy Kate Cowan-Sharp', 'date': date, 'bio': bio}
+	js = json.dumps(resp)
+	self.set_header("Content-Type", "application/json")
+	self.write(js)
+	return
 
 class ContactHandler(tornado.web.RequestHandler):
     def get(self):
@@ -42,7 +52,7 @@ class ContactHandler(tornado.web.RequestHandler):
 	js = json.dumps(contact_info)
 	self.set_header("Content-Type", "application/json")
 	self.write(js)
-
+	return
 
 
 class ResumeHandler(tornado.web.RequestHandler):
@@ -52,9 +62,9 @@ class ResumeHandler(tornado.web.RequestHandler):
 
 application = tornado.web.Application([
         (r'/', IndexHandler),
-        (r'/bio', BioHandler),
-        (r'/contact', ContactHandler),
-        (r'/resume', ResumeHandler),
+        (r'/bio.json', BioHandler),
+        (r'/contact.json', ContactHandler),
+        (r'/resume.json', ResumeHandler),
         ])
 
 if __name__ == '__main__':
