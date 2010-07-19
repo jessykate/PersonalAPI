766655d5c9bea1e25223c17fda85c79707ff3ecd
jessykate <jessy.cowansharp@gmail.com>
2010-07-19 12:45:27 -0400

    pulling out data and settings

---
+++ data.example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# file to contain data served by the PersonalAPI. copy and save this file as
+# data.py
+
+NAME = "Billy Bob"
+BIO_MAIN = "A very interesting paragraph"
+BIO_WORK = "Something more work specific"
+TWITTER = "yourhandle"
+PHONE_MOBILE = "111-222-3333"
+EMAIL_MAIN = "user@example.com"
+ 
---
+++ settings.example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+# place app specific settings in her. 
+
+BASE_URL = "http://example.com"
+
+
